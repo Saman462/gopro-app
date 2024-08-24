1577c5bed5a17915ee85d5460607222905985635
Saman462 <szohra.bese-26mcs@student.nust.edu.pk>
2024-08-24 18:24:01 +0500

    pwa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GoProConnectionWrapper from './GoProConnectionWrapper';
+import MediaListPage from './MediaListPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<GoProConnectionWrapper />} />
+          <Route path="/media" element={<MediaListPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
