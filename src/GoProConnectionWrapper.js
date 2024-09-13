51e1027eba9e55fb0c8d9e6481a6c1b7f6769fc7
Saman462 <szohra.bese-26mcs@student.nust.edu.pk>
2024-09-13 22:25:09 +0500

    new

--- src/GoProConnectionWrapper.js
+++ src/GoProConnectionWrapper.js
@@ -370,7 +370,7 @@ const CheckGoProConnection = () => {
             setLoading(true);
             try {
                 // Use the proxied URL instead of the direct GoPro URL
-                const response = await axios.get('https://proxy-server-pink.vercel.app/api/camera/state');
+                const response = await axios.get('https://proxy-server-pink.vercel.app/api/api/camera/state');
                 if (response.status === 200) {
                     setIsConnected(true);
                 } else {
